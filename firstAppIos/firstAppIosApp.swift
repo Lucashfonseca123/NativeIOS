428bf98906620354bccdbb5ee41d49ec937c0ceb
Lucas Fonseca <lucas.fonseca@franq.com.br>
2021-08-07 23:16:43 -0300

    Initial Commit

---
+++ firstAppIos/firstAppIosApp.swift
@@ -0,0 +1,17 @@
+//
+//  firstAppIosApp.swift
+//  firstAppIos
+//
+//  Created by Lucas Fonseca on 07/08/21.
+//
+
+import SwiftUI
+
+@main
+struct firstAppIosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
