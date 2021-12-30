052f702c8a9cd3b9197356e8bfa20f80315f10e2
Lucas <lucas_hfonseca@yahoo.com.br>
2021-12-29 22:26:16 -0300

    feat: Add switch to filter favorite places

--- firstAppIos/Views/LandmarkList.swift
+++ firstAppIos/Views/LandmarkList.swift
@@ -8,27 +8,35 @@
 import SwiftUI
 
 struct LandmarkList: View {
+    @State private var showFavoriteOnly = false
+    
+    var filteredLandmarks: [Landmark] {
+        landmarks.filter { landmark in
+            (!showFavoriteOnly || landmark.isFavorite)
+        }
+    }
+    
     var body: some View {
         NavigationView {
-            List(landmarks){ landmark in
-                NavigationLink{
-                    LandmarkDetail(landmark: landmark)
-                } label: {
-                    LandmarkRow(landmark: landmark)
+            List {
+                Toggle(isOn: $showFavoriteOnly){
+                    Text("Favorites only")
                 }
+                ForEach(filteredLandmarks){ landmark in
+                    NavigationLink{
+                        LandmarkDetail(landmark: landmark)
+                    } label: {
+                        LandmarkRow(landmark: landmark)
+                    }
+                }
+                .navigationTitle("Landmarks")
             }
-            .navigationTitle("Landmarks")
         }
     }
 }
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
-        ForEach(["iPhone SE (2nd generation)", "iPhone XS Max"], id: \.self) { deviceName in
-                   LandmarkList()
-                       .previewDevice(PreviewDevice(rawValue: deviceName))
-                       .previewDisplayName(deviceName)
-               }
-                
+        LandmarkList()
     }
 }
